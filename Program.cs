bd1d27c3d670527c7d14d6e12c37c7490c46a9e0
fabricio <fnmamao@gmail.com>
2020-05-15 15:50:05 -0300

    exercicio

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercicio_ola_usuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # exercicio_ola_usuario
+[console: OlaUsuario] Faça um programa solicite que o usuário digite seu nome. Exiba Olá, ___! (na lacuna ___ exiba o nome digitado).
+
