7db2e2760f23a3e3d3263fa81e18b26f10f29820
fabricio <fnmamao@gmail.com>
2020-05-15 15:51:56 -0300

    exercicio

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace exercicio_ola_usuario
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             string nome;
+            Console.WriteLine("digite seu nome:");
+            nome = Console.ReadLine();
+            Console.WriteLine("olá " + nome);
         }
     }
 }
